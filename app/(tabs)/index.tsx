6c33557204ce19a9bf773fb1113a1f0e6f41a298
Tony <tonyjays@yahoo.com>
2025-04-14 07:04:43 +0100

    Modified the index page to reflect no internet connection

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -5,6 +5,7 @@ import Rating from "../../components/Rating";
 import { useGetProductsQuery } from "@/hooks/productHooks";
 import NetInfo from "@react-native-community/netinfo";
 import AnimatedNoInternetBanner from "@/components/AnimatedNoInternetBanner";
+
 import {
   View,
   Text,
@@ -28,115 +29,132 @@ export default function Index() {
 
     return () => unsubscribe();
   }, []);
-
   const filteredProducts = (products || []).filter((product) =>
     product.name.toLowerCase().includes(searchQuery.toLowerCase())
   );
-  return isLoading ? (
-    <ActivityIndicator
-      size="large"
-      color="#0000ff"
-      style={{ flex: 1, alignItems: "center", justifyContent: "center" }}
-    />
-  ) : error ? (
-    <View
-      style={{
-        flex: 1,
-        alignItems: "center",
-        justifyContent: "center",
-      }}
-    >
-      {" "}
-      <Text>Error fetching data</Text>
-    </View>
-  ) : (
+  {
+    !isConnected && (
+      <View style={{ flex: 1 }}>
+        <StatusBar barStyle="light-content" backgroundColor="#FFA500" />
+        <AnimatedNoInternetBanner visible={!isConnected} />
+        <View
+          style={{
+            flex: 1,
+            justifyContent: "center",
+            alignItems: "center",
+            backgroundColor: "#fff",
+          }}
+        >
+          <ActivityIndicator size="large" color="#FFA500" />
+        </View>
+      </View>
+    );
+  }
+
+  return (
     <View style={{ flex: 1 }}>
       <StatusBar barStyle="light-content" backgroundColor="#FFA500" />
       <AnimatedNoInternetBanner visible={!isConnected} />
-      <View
-        style={{
-          backgroundColor: "#ff9900",
-          marginBottom: 10,
-          padding: 10,
-          position: "relative",
-          display: "flex",
-        }}
-      >
-        <Ionicons
-          name={"search"}
-          size={22}
-          color={"gray"}
-          style={{ position: "absolute", left: 15, top: 15, zIndex: 1 }}
+
+      {isLoading ? (
+        <ActivityIndicator
+          size="large"
+          color="#0000ff"
+          style={{ flex: 1, alignItems: "center", justifyContent: "center" }}
         />
-        <TextInput
-          style={{
-            backgroundColor: "white",
-            width: "80%",
-            height: 35,
-            borderRadius: 15,
-            paddingLeft: 30,
-          }}
-          placeholder="Search"
-          value={searchQuery}
-          onChangeText={setSearchQuery}
-        />
-      </View>
-      {filteredProducts.length > 0 ? (
-        <FlatList
-          data={filteredProducts}
-          style={{ padding: 10 }}
-          numColumns={2}
-          keyExtractor={(item) => item._id}
-          renderItem={({ item }) => (
-            <>
-              <TouchableOpacity
-                style={{
-                  flex: 1,
-                  margin: 5,
-                  backgroundColor: "#fff",
-                  borderRadius: 10,
-                  padding: 10,
-                  shadowColor: "#000",
-                  shadowOpacity: 0.1,
-                  shadowOffset: { width: 0, height: 2 },
-                  elevation: 3,
-                }}
-              >
-                <Link
-                  href={{
-                    pathname: "/productdetailscreen",
-                    params: { slug: `${item.slug}` },
+      ) : error ? (
+        <View
+          style={{ flex: 1, alignItems: "center", justifyContent: "center" }}
+        >
+          <Text>Error fetching data</Text>
+        </View>
+      ) : (
+        <>
+          <View
+            style={{
+              backgroundColor: "#ff9900",
+              marginBottom: 10,
+              padding: 10,
+              position: "relative",
+              display: "flex",
+            }}
+          >
+            <Ionicons
+              name={"search"}
+              size={22}
+              color={"gray"}
+              style={{ position: "absolute", left: 15, top: 15, zIndex: 1 }}
+            />
+            <TextInput
+              style={{
+                backgroundColor: "white",
+                width: "80%",
+                height: 35,
+                borderRadius: 15,
+                paddingLeft: 30,
+              }}
+              placeholder="Search"
+              value={searchQuery}
+              onChangeText={setSearchQuery}
+            />
+          </View>
+
+          {filteredProducts.length > 0 ? (
+            <FlatList
+              data={filteredProducts}
+              style={{ padding: 10 }}
+              numColumns={2}
+              keyExtractor={(item) => item._id}
+              renderItem={({ item }) => (
+                <TouchableOpacity
+                  style={{
+                    flex: 1,
+                    margin: 5,
+                    backgroundColor: "#fff",
+                    borderRadius: 10,
+                    padding: 10,
+                    shadowColor: "#000",
+                    shadowOpacity: 0.1,
+                    shadowOffset: { width: 0, height: 2 },
+                    elevation: 3,
                   }}
                 >
-                  <View>
-                    <Image
-                      source={{ uri: item.image }}
-                      style={{ width: 150, height: 150, borderRadius: 10 }}
-                      resizeMode="contain"
-                    />
-                  </View>
+                  <Link
+                    href={{
+                      pathname: "/productdetailscreen",
+                      params: { slug: `${item.slug}` },
+                    }}
+                  >
+                    <View>
+                      <Image
+                        source={{ uri: item.image }}
+                        style={{ width: 150, height: 150, borderRadius: 10 }}
+                        resizeMode="contain"
+                      />
+                    </View>
 
-                  <Text style={{ fontWeight: "bold", marginTop: 5 }}>
-                    {item.name}
-                  </Text>
-                  <Rating rating={item.rating} numReviews={item.numReviews} />
-                  <Text>${item.price}</Text>
-                </Link>
-              </TouchableOpacity>
-            </>
+                    <Text style={{ fontWeight: "bold", marginTop: 5 }}>
+                      {item.name}
+                    </Text>
+                    <Rating rating={item.rating} numReviews={item.numReviews} />
+                    <Text>${item.price}</Text>
+                  </Link>
+                </TouchableOpacity>
+              )}
+            />
+          ) : (
+            <Text
+              style={{
+                justifyContent: "center",
+                alignItems: "center",
+                textAlign: "center",
+                color: "gray",
+              }}
+            >
+              No products
+            </Text>
           )}
-        />
-      ) : (
-        <Text
-          style={{
-            justifyContent: "center",
-            alignItems: "center",
-            textAlign: "center",
-            color: "gray",
-          }}
-        >
-          No products
-        </Text>
+        </>
       )}
     </View>
   );
